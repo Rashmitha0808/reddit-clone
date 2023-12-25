851837949dca62177e39c96fb23f01f263c5bb14
Rashmitha <rashmikulal831@gmail.com>
2023-12-26 00:15:09 +0530

    changed id

--- src/Components/LogInSignUp/LoginPage.jsx
+++ src/Components/LogInSignUp/LoginPage.jsx
@@ -57,20 +57,3 @@ export default LoginPage;
 {
   /* <GoogleLoginButton onSuccess={handleGoogleLoginSuccess} onFailure={handleGoogleLoginFailure} /> */
 }
-
-// Client ID
-// 113413976761-82vj3sorkp2a1f71u3t5bhn1qk931f9u.apps.googleusercontent.com
-// Client secret
-// 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
-// Creation date
-// 25 December 2023 at 13:30:01 GMT+5
-// Status
-// Enabled
-
-// {"web":{"client_id":"113413976761-82vj3sorkp2a1f71u3t5bhn1qk931f9u.apps.googleusercontent.com",
-// "project_id":"reddit-clone-409207",
-// "auth_uri":"https://accounts.google.com/o/oauth2/auth",
-// "token_uri":"https://oauth2.googleapis.com/token",
-// "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
-// "client_secret":"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",
-// "javascript_origins":["http://localhost:5173"]}}
